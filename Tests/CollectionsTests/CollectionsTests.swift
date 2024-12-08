94e2ddef59b82fedf039204d44f4c98115603df4
Piyush <piyush@Mac1.lan>
2024-12-08 17:21:55 +0530

    Added 2D List data-structure & tests for the same.

--- Tests/CollectionsTests/CollectionsTests.swift
+++ Tests/CollectionsTests/CollectionsTests.swift
@@ -1,6 +1,27 @@
-import Testing
 @testable import Collections
+import Foundation
+import Testing
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+// swiftlint:disable no_magic_numbers
+@Test
+internal func test0001() async throws {
+	let list: List<String> = .init(
+		rows: 20,
+		columns: 30
+	)
+
+	list[15, 16] = "15_16"
+
+	#expect(list[15, 16] == "15_16")
+	#expect(list[16, 15] == nil)
+
+	var items: [String] = []
+	list.forEach { _, _, item in
+		if let item {
+			items.append(item)
+		}
+	}
+
+	#expect(items.count == 1)
 }
+// swiftlint:enable no_magic_numbers
